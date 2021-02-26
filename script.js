04ec166ad26ed6dcb5c6408bb88d8ed2ff8f13a9
Sergey Minenkov <minenkov.sergey42@gmail.com>
2021-02-26 13:29:51 +0300

    final touches

---
+++ script.js
@@ -0,0 +1,43 @@
+$(function () {
+    let colorType = "background-color"
+    $("#colorButton").on("click", function () {
+        colorType = "color"
+    });
+    $("#backgroundColorButton").on("click", function () {
+        colorType = "background-color"
+    });
+    function hexFromRGB(r, g, b) {
+        var hex = [
+            r.toString(16),
+            g.toString(16),
+            b.toString(16)
+        ];
+        $.each(hex, function (nr, val) {
+            if (val.length === 1) {
+                hex[nr] = "0" + val;
+            }
+        });
+        return hex.join("").toUpperCase();
+    }
+    function refreshSwatch() {
+        var red = $("#red").slider("value"),
+            green = $("#green").slider("value"),
+            blue = $("#blue").slider("value"),
+            hex = hexFromRGB(red, green, blue);
+        hexText = hexFromRGB(red + 10, green + 10, blue + 10);
+        $("#swatch").css(colorType, "#" + hex)
+        //$("#swatch").css("color", "#" + hexText)
+    }
+
+    $("#red, #green, #blue").slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 127,
+        slide: refreshSwatch,
+        change: refreshSwatch
+    });
+    $("#red").slider("value", 255);
+    $("#green").slider("value", 140);
+    $("#blue").slider("value", 60);
+});
